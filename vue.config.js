88484750913e61cacb97a17af2ac1c145e8fa382
NWUzmed <78712654+nwuzmedoutlook@users.noreply.github.com>
2023-03-27 09:26:08 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -38,7 +38,7 @@ module.exports = {
       entry: 'src/main.js',
       template: 'public/index.html',
       filename: 'index.html',
-      title: 'YesPlayMusic',
+      title: 'DAC音乐库',
       chunks: ['main', 'chunk-vendors', 'chunk-common', 'index'],
     },
   },
